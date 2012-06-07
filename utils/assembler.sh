5629ad65f435fca73a6871b0c2ff387c3a9923ea
Zju Toe <zjutoe@gmail.com>
2012-06-07 08:18:27 +0800

    use lua script to convert reg nick to reg number
    Signed-off-by: Zju Toe <zjutoe@gmail.com>

--- utils/assembler.sh
+++ utils/assembler.sh
@@ -1,16 +1,20 @@
-#!/bin/sh
+#!/bin/sh -x
+
+regno() {
+    ./lua52 regno.lua $1
+}
 
 do_r_type() {
     
     echo "R-type" $op $rd  $rs $rt
 
     b_op='000000'
-    b_rs=regno $rs
-    b_rt=regno $rt
-    b_rd=regno $rd
+    b_rs=$(regno $rs)
+    b_rt=$(regno $rt)
+    b_rd=$(regno $rd)
     b_sa='00000'
     
-    
+    echo $b_op $b_rs $b_rt $b_rd
 
 }
 
---
+++ utils/regno.lua
@@ -0,0 +1,9 @@
+regs = {}
+regs['zero'] = 0
+regs['t0'] = 4
+regs['t1'] = 5
+regs['t2'] = 6
+regs['t3'] = 7
+
+
+print(regs[arg[1]])
