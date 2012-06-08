e3c04a098c87577944517beccf0fc96e288fc1bb
Zju Toe <zjutoe@gmail.com>
2012-06-08 23:27:24 +0800

    R-type translation done
    Signed-off-by: Zju Toe <zjutoe@gmail.com>

--- utils/assembler.sh
+++ utils/assembler.sh
@@ -1,20 +1,25 @@
-#!/bin/sh -x
+#!/bin/sh
 
 regno() {
     ./lua52 regno.lua $1
 }
 
+r_type_func() {
+    ./lua52 r_type_func.lua $1
+}
+
 do_r_type() {
     
-    echo "R-type" $op $rd  $rs $rt
+    #echo "R-type" $op $rd  $rs $rt
 
     b_op='000000'
     b_rs=$(regno $rs)
     b_rt=$(regno $rt)
     b_rd=$(regno $rd)
     b_sa='00000'
+    b_func=$(r_type_func $op)
     
-    echo $b_op $b_rs $b_rt $b_sa $b_rd
+    echo "$b_op""$b_rs""$b_rt""$b_sa""$b_rd""$b_func"
 
 }
 
