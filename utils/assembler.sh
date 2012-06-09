da209140b372a7bac7fc2a0133241f1ea82b66f3
Zju Toe <zjutoe@gmail.com>
2012-06-09 10:12:02 +0800

    print segmented 4x8 for 32bit inst, and in hex form as well
    Signed-off-by: Zju Toe <zjutoe@gmail.com>

--- utils/assembler.sh
+++ utils/assembler.sh
@@ -8,6 +8,16 @@ r_type_func() {
     ./lua52 r_type_func.lua $1
 }
 
+inst_hex() {
+    read -r s0 s1 s2 s3 s4 s5 s6 s7 <<< $1
+    for s in  $s0 $s1 $s2 $s3 $s4 $s5 $s6 $s7; do
+	h="$(./lua52 base_bin2hex.lua $s)"
+	inst_hex="$inst_hex$h"
+    done
+    
+    echo "$inst_hex"
+}
+
 do_r_type() {
     
     #echo "R-type" $op $rd  $rs $rt
@@ -19,8 +29,11 @@ do_r_type() {
     b_sa='00000'
     b_func=$(r_type_func $op)
     
-    echo "$b_op""$b_rs""$b_rt""$b_sa""$b_rd""$b_func"
+    inst="$b_op""$b_rs""$b_rt""$b_sa""$b_rd""$b_func"
+    inst_seg="$(cut -c 1-4 <<< $inst) $(cut -c 5-8 <<< $inst) $(cut -c 9-12 <<< $inst) $(cut -c 13-16 <<< $inst)\
+         $(cut -c 17-20 <<< $inst) $(cut -c 21-24 <<< $inst) $(cut -c 25-28 <<< $inst) $(cut -c 29-32 <<< $inst)"
 
+    echo $inst -- $inst_seg -- $(inst_hex "$inst_seg")
 }
 
 
---
+++ utils/base_bin2hex.lua
@@ -0,0 +1,4 @@
+dofile('base_conv.lua')
+
+print( bin2hex[arg[1]] )
+
